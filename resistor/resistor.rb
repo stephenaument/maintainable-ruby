64eed323c84ee9c97283c13d33f8a24e62b291b8
Stephen Aument <saument@gmail.com>
2019-10-23 15:02:13 -0500

    tdd: continue building tolerance method

--- resistor/resistor.rb
+++ resistor/resistor.rb
@@ -1,5 +1,21 @@
 class Resistor
+  def initialize(*colors)
+    @tolerance_color = colors.last
+  end
+
   def tolerance
-    '±5%'
+    color_values = {
+      'brown'  => '±1%',
+      'red'    => '±2%',
+      'green'  => '±0.5%',
+      'blue'   => '±0.25%',
+      'violet' => '±0.1%',
+      'grey'   => '±0.05%',
+      'gray'   => '±0.05%',
+      'gold'   => '±5%',
+      'silver' => '±10%',
+    }
+
+    color_values[@tolerance_color]
   end
 end
--- resistor/resistor_spec.rb
+++ resistor/resistor_spec.rb
@@ -2,11 +2,45 @@ require 'minitest/autorun'
 require_relative './resistor'
 
 describe Resistor do
-  let(:subject) { Resistor.new }
+  let(:subject) { Resistor.new(*%w[green blue yellow gold]) }
 
   describe '#tolerance' do
-    it 'returns the correct value' do
-      expect(subject.tolerance).must_equal '±5%'
+    describe 'when given 4 colors' do
+      it 'returns the correct value' do
+        expect(subject.tolerance).must_equal '±5%'
+      end
+    end
+
+    describe 'when given 5 colors' do
+      let(:subject) { Resistor.new(*%w[red orange violet black brown]) }
+
+      it 'returns the correct value' do
+        expect(subject.tolerance).must_equal '±1%'
+      end
+    end
+
+    describe 'all the colors' do
+      let(:tolerance_values) do
+        {
+          'brown' => '±1%',
+          'red' => '±2%',
+          'green' => '±0.5%',
+          'blue' => '±0.25%',
+          'violet' => '±0.1%',
+          'grey' => '±0.05%',
+          'gray' => '±0.05%',
+          'gold' => '±5%',
+          'silver' => '±10%',
+        }
+      end
+
+      it 'just works' do
+        tolerance_values.each do |color, tolerance|
+          subject = Resistor.new('green', 'blue', 'yellow', color)
+
+          expect(subject.tolerance).must_equal tolerance
+        end
+      end
     end
   end
 end
