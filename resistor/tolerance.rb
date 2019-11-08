f6a222321f0337d9f02c7cf877b37394db090b84
Stephen Aument <saument@gmail.com>
2019-11-08 13:01:34 -0600

    tdd: refactor tolerance class

--- resistor/tolerance.rb
+++ resistor/tolerance.rb
@@ -1,26 +1,49 @@
 class Tolerance
+  COLOR_VALUES = {
+    'brown'  => '±1%',
+    'red'    => '±2%',
+    'green'  => '±0.5%',
+    'blue'   => '±0.25%',
+    'violet' => '±0.1%',
+    'grey'   => '±0.05%',
+    'gray'   => '±0.05%',
+    'gold'   => '±5%',
+    'silver' => '±10%',
+  }
+
   attr_reader :color
 
   def initialize(color)
     @color = color
 
-    known_tolerance_colors = %w[brown red green blue violet gray grey gold silver]
-    raise ArgumentError, "invalid tolerance color given: #{@color}" unless known_tolerance_colors.include? @color
+    Validator.new(self).validate!
   end
 
   def to_s
-    color_values = {
-      'brown'  => '±1%',
-      'red'    => '±2%',
-      'green'  => '±0.5%',
-      'blue'   => '±0.25%',
-      'violet' => '±0.1%',
-      'grey'   => '±0.05%',
-      'gray'   => '±0.05%',
-      'gold'   => '±5%',
-      'silver' => '±10%',
-    }
+    COLOR_VALUES[@color]
+  end
 
-    color_values[@color]
+  class Validator
+    attr_reader :tolerance
+
+    def initialize(tolerance)
+      @tolerance = tolerance
+    end
+
+    def color
+      tolerance.color
+    end
+
+    def known_colors
+      COLOR_VALUES.keys
+    end
+
+    def message
+      "invalid tolerance color given: #{color}"
+    end
+
+    def validate!
+      raise ArgumentError, message unless known_colors.include? color
+    end
   end
 end
